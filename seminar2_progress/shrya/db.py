80bdc7bc7517661b456ac8d0f72421a03c4c73c7
jayvora9 <jay,vora@sakec.ac.in>
2021-05-21 23:53:17 +0530

    pusj

--- seminar2_progress/shrya/db.py
+++ seminar2_progress/shrya/db.py
@@ -58,7 +58,7 @@ def populate(c):
 
         LOGIN_INFO = [(1, 'jay.vora@sakec.ac.in', 'jay'),
                        (2, 'gunjan.vora@sakec.ac.in', 'gunjan'),
-                       (3, 'kaushik.gami@sakec.ac.in', 'kaushik'),
+                       (3, 'kaushik.gami@sakec.ac.in', 'kau'),
                        (4, 'keval.shah@sakec.ac.in', 'keval'),
                       (5, 'rutvi.shah@sakec.ac.in', 'rutvi'),
                       (6, 'siddhi.rane@sakec.ac.in', 'siddhi'),
@@ -116,30 +116,5 @@ populate(c)
 conn.commit()
 
 
-def bulkDataIns():
-   print("DATA INSERTED")
-   for num in range(13,150):
-        # if (num%2)==0:
-            c.execute( "INSERT INTO LOGIN VALUES(?,?,?)",(num,'abc'+str(num)+'@sakec.ac.in','abc'+str(num)) )
-            # c.execute( "INSERT INTO STUD_INFO VALUES(?,?,?,?)", () )
-        # else:
-        #     c.execute("INSERT INTO LOGIN VALUES(?,?,?)",(num,'xyz'+str(num)+'@sakec.ac.in','xyz'+str(num)) )
-for num in range(13, 201):
-    # if (num % 2) == 0:
-    
-              c.execute("INSERT INTO LOGIN VALUES(?,?,?)", (num, 'abc' + str(num) + '@sakec.ac.in', 'abc' + str(num)))
-              c.execute( "INSERT INTO STUD_INFO VALUES(?,?,?,?)", (num, 'ABC' , 'DEF' , 8) )
-              c.execute("INSERT INTO GPA_DETAILS VALUES(?,?,?,?,?,?,?,?,?)", (num, 8.4, 8.3, 9.4, 8.9, 8.8, 9.5, 9.2, 0.0))
-    # else:
-    #           c.execute("INSERT INTO LOGIN VALUES(?,?,?)", (num, 'xyz' + str(num) + '@sakec.ac.in', 'xyz' + str(num)))
-    #           c.execute("INSERT INTO STUD_INFO VALUES(?,?,?,?)", (num, 'PQR', 'XYZ', 7))
-    
-    #           c.execute("INSERT INTO GPA_DETAILS VALUES(?,?,?,?,?,?,?,?,?)", (num, 8.6, 8.3, 9.0, 8.5, 8.4, 8.9, 0.0, 0.0))
 
 
-bulkDataIns()
-conn.commit()
-
-
-for num in range(13,15):
-         print(num,'abc'+str(num)+'@sakec.ac.in','abc'+str(num))
